71d1d54c4053529e56b231e77eb47b362c97c621
StoyanCh <stoqnchompov@gmail.com>
2022-11-28 10:43:31 +0200

    Има меню, фрагментите уж работятя

--- app/src/main/java/tu_varna/sit/mychecklist20/activities/MainActivity.kt
+++ app/src/main/java/tu_varna/sit/mychecklist20/activities/MainActivity.kt
@@ -1,10 +1,14 @@
 package tu_varna.sit.mychecklist20.activities
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import tu_varna.sit.mychecklist20.*
 import tu_varna.sit.mychecklist20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +25,21 @@ class MainActivity : AppCompatActivity() {
             binding.drawerLayout.openDrawer(GravityCompat.START)
         }
 
+        binding.navigationView.setNavigationItemSelectedListener {
+            it.isChecked = true
+            when(it.itemId){
+                R.id.myTasks -> replaceFragment(MyTasksFragment(),it.title.toString() )
+                R.id.toDo -> replaceFragment(ToDoFragment(), it.title.toString())
+                R.id.importantTasks -> replaceFragment(ImportantTasksFragment(), it.title.toString())
+                R.id.todayTasks -> replaceFragment(TodayTaskFragment(), it.title.toString())
+                R.id.calendar -> replaceFragment(TodayTaskFragment(), it.title.toString())
+                //  R.id.menuSetting -> replaceFragment(SettingsFragment(), it.title.toString())
+            }
+            true
+
+        }
+
+
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(toggle.onOptionsItemSelected(item)){
@@ -28,5 +47,15 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun replaceFragment(fragment: Fragment, title : String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.drawerLayout, fragment)
+        fragmentTransaction.commit()
+        binding.drawerLayout.closeDrawers()
+        setTitle(title)
+    }
+
 }
 
