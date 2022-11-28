2800793ca3a82982d4c90f83a273a598f66131e9
StoyanCh <stoqnchompov@gmail.com>
2022-11-28 11:27:07 +0200

    Има меню, фрагментите уж работятя

--- app/src/main/java/tu_varna/sit/mychecklist20/fragments/importantTasksFragment.kt
+++
@@ -1,8 +0,0 @@
-package tu_varna.sit.mychecklist20.fragments
-
-import androidx.fragment.app.Fragment
-import tu_varna.sit.mychecklist20.R
-
-class importantTasksFragment : Fragment(R.layout.fragment_important_tasks) {
-
-}
